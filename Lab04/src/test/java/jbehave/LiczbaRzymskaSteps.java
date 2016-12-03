12b20405303bba8ae88eee8044ca641fcd86f0a2
M <M@XPS-13>
2016-12-03 13:52:13 +0100

    LiczbaRzymskaSteps amendment

--- Lab04/src/test/java/jbehave/LiczbaRzymskaSteps.java
+++ Lab04/src/test/java/jbehave/LiczbaRzymskaSteps.java
@@ -14,7 +14,7 @@ public class LiczbaRzymskaSteps {
 			liczbaRzymska = new LiczbaRzymska(liczba);
 	}
 	@Then("the result should be $roman")
-	public void ShouldBeString(String roman){
+	public void ShouldBeRoman(String roman){
 		assertEquals(roman, liczbaRzymska.toString());
 	}
 }
